845c4d9f3acadceff36fe878763e3e98805e6c5f
foldesizoltan14 <43464693+foldesizoltan14@users.noreply.github.com>
2019-09-25 10:11:34 +0200

    Add files via upload

---
+++ arroway/gagyi/hamis.java
@@ -0,0 +1,11 @@
+class igaz{
+	public static void main(String args[])
+	{
+		Integer t = 100;
+		Integer x = 100;
+
+		while(x <= t && x>=t &&  t != x)
+			System.out.println("hamis");
+		}
+
+}
---
+++ arroway/yoda/yoda.java
@@ -0,0 +1,10 @@
+class yoda {
+ 
+public static void main(String args[]){
+String mine = null;
+if ( mine.equals("yoda")){ System.out.println("nem fog működni");
+
+}
+}
+
+}
